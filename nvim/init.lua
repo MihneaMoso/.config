417f1a15f3731e66d3c3cbe6d49e5d93a21a7840
Mihnea Moso <mihnea.mosorescu@gmail.com>
2026-04-11 13:06:21 +0300

    Changes detected in dconf/user

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,6 +17,7 @@ vim.o.undodir = os.getenv("HOME") .. "/.cache/nvim/undodir"
 vim.o.list = true
 --vim.o.listchars = "tabs:> "
 vim.o.path = "**"
+vim.opt.mouse = 'a'
 
 vim.g.neovide_cursor_animation_length = 0
 
@@ -65,12 +66,19 @@ map("n", "<leader>x", ":copen<CR>")
 map("n", "<leader>c", ":!ctags -R .<CR>")
 
 -- Neovide pasting
-if vim.g.neovide then
-  vim.keymap.set("v", "<C-c>", '"+y', { desc = "Copy to clipboard" })
+--if vim.g.neovide then
+--  vim.keymap.set("v", "<C-c>", '"+y', { desc = "Copy to clipboard" })
+--  vim.keymap.set({ "n", "i", "v", "c", "t" }, "<C-v>", function()
+--    vim.api.nvim_paste(vim.fn.getreg("+"), true, -1)
+--  end, { desc = "Paste from clipboard", silent = true })
+--end
+
+-- Regular pasting
+vim.keymap.set("v", "<C-c>", '"+y', { desc = "Copy to clipboard" })
   vim.keymap.set({ "n", "i", "v", "c", "t" }, "<C-v>", function()
     vim.api.nvim_paste(vim.fn.getreg("+"), true, -1)
   end, { desc = "Paste from clipboard", silent = true })
-end
+
 
 --map('n', '<C-h>', ':bprevious<CR>', { desc = 'Previous buffer' })
 --map('n', '<C-l>', ':bnext<CR>', { desc = 'Next buffer' })
@@ -85,3 +93,60 @@ map('n', '<C-w><C-Down>', '<C-w>j')
 map('n', '<C-w><C-Left>', '<C-w>h')
 map('n', '<C-w><C-Right>', '<C-w>l')
 
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+-- vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- add your plugins here
+    {
+        'mg979/vim-visual-multi',
+        branch = 'master',
+    },
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
+
+-- Multi-cursor Configuration
+vim.g.VM_maps = {
+    -- Mapping Ctrl+Shift+Arrows for Normal and Visual mode
+    ['Add Cursor Down']             = '<C-S-Down>',
+    ['Add Cursor Up']               = '<C-S-Up>',
+    -- Note: vim-visual-multi treats Left/Right slightly differently 
+    -- but we can map them to 'Add Cursor At Pos' logic
+    ['Add Cursor Left']             = '<C-S-Left>',
+    ['Add Cursor Right']            = '<C-S-Right>',
+}
+
+-- Mouse Support: Ctrl+Shift+Click to add a cursor
+vim.keymap.set('n', '<C-S-LeftMouse>', '<Plug>(VM-Mouse-Cursor)', { desc = 'Add cursor at click' })
+
+-- Escape behavior
+-- By default, vim-visual-multi uses <Esc> or 'q' to exit.
+-- If it doesn't feel snappy, you can explicitly map it:
+vim.g.VM_quit_after_leaving_insert_mode = 0 -- Keep cursors when leaving insert mode
