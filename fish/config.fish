cc5b094c26abad5cf9a4043db135cecd56a1efb2
Mihnea Moso <mihnea.mosorescu@gmail.com>
2026-04-11 18:32:00 +0300

    Changes detected in fish/config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -9,3 +9,7 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
+set -Ux --export SSL_CERT_FILE '/etc/ssl/certs/ca-certificates.crt'
+
+# aliases
+alias curl="curl -k"
