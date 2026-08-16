54a8b950fb06128b2df2147bb56dd403a39662d2
Mihnea Moso <mihnea.mosorescu@gmail.com>
2026-08-16 22:17:05 +0300

    Changes detected in configstore/update-notifier-omniroute.json

--- fish/config.fish
+++ fish/config.fish
@@ -10,6 +10,7 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 set --export BUN_INSTALL "$HOME/.bun"
 set --export PATH $BUN_INSTALL/bin $PATH
 set --export OPENAI_BASE_URL "http://localhost:20128/v1"
+set --export OMNIROUTE_API_KEY "sk-349d7260628f235b-87224b-50fb20d3"
 set --export OPENAI_API_KEY "local-key"
 set -Ux --export SSL_CERT_FILE '/etc/ssl/certs/ca-certificates.crt'
 
